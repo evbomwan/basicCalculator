f071348df45cd2012b0d116b08e28dbdd81cc980
evbomwan <evbomwanc@gmail.com>
2026-05-24 11:58:16 +0100

    created the ui for the calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+
+let num1;
+let num2;
+let operator;
+
+function operate(operator,num1,num2){
+    add();
+}
+function updateNumberVariable(){
+    
+}
